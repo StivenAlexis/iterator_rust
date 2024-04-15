84724ecadc6cd413cc78152156d36206ae82b467
Stiven Alexis <stiven22monsalvo@gmail.com>
2024-04-15 10:38:54 -0300

    test_actions_failed

--- src/tests/iterator_test.rs
+++ src/tests/iterator_test.rs
@@ -7,7 +7,7 @@ fn iterator_test() {
     let mut task_list = TaskList::new();
     task_list.add_task(Task {id:1 ,description: "hacer el aseo".to_string() });
     task_list.add_task(Task {id:2, description: "lavar los platos ".to_string() });
-    let mut iterator = task_list.get_iterator();
+    let mut iterator = task_list.get_iterator;
 
 
     assert_eq!(iterator.next().unwrap().description, "hacer el aseo");
